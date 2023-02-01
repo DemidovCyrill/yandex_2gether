f99414da6d1efecd238fcbafe422dad1fa0619d1
DemidovCyrill <118429206+DemidovCyrill@users.noreply.github.com>
2023-02-01 20:09:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -38,6 +38,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             break
+        elif event.type == pygame.KEYDOWN:
+            print(event.key)
+            if event.key == 1073741906 and float(lon) < 90:
+                lat = str(float(lat) + 0.5)
+            elif event.key == 1073741905 and float(lon) > 0:
+                lat = str(float(lat) - 0.5)
+            elif event.key == 1073741904:
+                lon = str(float(lon) - 0.5)
+            elif event.key == 1073741903:
+                lon = str(float(lon) + 0.5)
+            elif event.key == 1073741899:
+                delta = str(float(delta) - 0.01)
+            elif event.key == 1073741902:
+                delta = str(float(delta) + 0.01)
 
     map_img = get_map_img()
     window.blit(map_img, (0, 0))
